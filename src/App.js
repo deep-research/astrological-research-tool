ffdfa87b8e826d8f821a9a7ab45e70cc6619d7de
Victor <deep-research>
2018-02-12 20:30:39 -0600

    api file added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import HomePage from "./components/HomePage";
+import HomePage from "./pages/HomePage";
 import './App.css';
 
 class App extends Component {
