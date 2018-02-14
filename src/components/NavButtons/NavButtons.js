e05da9a2308aa93d2b1f16db1d1ccd5416cfdeea
Victor <deep-research>
2018-02-13 21:01:43 -0600

    Login message aligned

--- src/components/NavButtons/NavButtons.js
+++ src/components/NavButtons/NavButtons.js
@@ -1,32 +1,29 @@
 import React, {Component} from "react";
-import { Link } from "react-router-dom";
 import "./NavButtons.css";
 
 class NavButtons extends Component {
     render() {
         return (
-        <div>
-            <ul className="nav nav-pills justify-content-center">
-                <li className="nav-item">
-                    <button type="button" className="btn btn-outline-primary btn-lg">About</button>
-                </li>
-                <li className="nav-space"></li>                
-                <li className="nav-item">
-                    <button type="button" className="btn btn-outline-primary btn-lg">Login</button>
-                </li>
-                <li className="nav-space"></li>    
-                <li className="nav-item">
-                    <button type="button" className="btn btn-outline-primary btn-lg">Register</button>
-                </li>
-                <li className="nav-space"></li>   
-                <li className="nav-item">
-                    <p id="loginMessage" className="nav-item navbar-text" >Login to save events</p>
-                </li>
-            </ul>
-          
-          {/* Add links */}       
-          {/* <Link to={`./index.html`}>click here</Link><br /> */}
-          {/* <Link to={`./`}>click here</Link> */}
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-3"></div>
+                <div className="col-sm-12 col-md-6">
+                    <ul className="nav nav-pills justify-content-center">
+                        <li className="nav-item">
+                            <button type="button" className="btn btn-outline-primary btn-lg">About</button>
+                        </li>
+                        <li className="nav-space"></li>                
+                        <li className="nav-item">
+                            <button type="button" className="btn btn-outline-primary btn-lg">Login</button>
+                        </li>
+                        <li className="nav-space"></li>
+                        <li className="nav-item">
+                            <button type="button" className="btn btn-outline-primary btn-lg">Register</button>
+                        </li>
+                    </ul>
+                </div>
+                <p id="loginMessage" className="nav-item navbar-text col-md-3" >Login to save events</p>
+            </div>
         </div>
         );
     }
