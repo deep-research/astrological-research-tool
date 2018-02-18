c1ad4718a966660955a3336d30a38a7f1ea3ca48
Victor <deep-research>
2018-02-18 16:20:16 -0600

    Google Timezone Conversion

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,13 @@
 import React, {Component} from "react";
 // import { Link } from "react-router-dom";
 import "./HomePage.css";
-import Navbar from "../../components/Navbar"
-import NavButtons from "../../components/NavButtons"
-import AboutSection from "../../components/AboutSection"
-import EventForm from "../../components/EventForm"
-import EventDisplay from "../../components/EventDisplay"
-import cities from "../../utils/cities.json"
+import Navbar from "../../components/Navbar";
+import NavButtons from "../../components/NavButtons";
+import AboutSection from "../../components/AboutSection";
+import EventForm from "../../components/EventForm";
+import EventDisplay from "../../components/EventDisplay";
+import cities from "../../utils/cities.json";
+import timezoner from "timezoner";
 
 class HomePage extends Component {
     state = {
@@ -28,9 +29,9 @@ class HomePage extends Component {
                 if (name === "cityInput") {
                     const userCity = this.state.cityInput.toLowerCase();
                     
-                    var indexOfCity = cities.map((x) => 
+                    var indexOfCity = cities.map((x) =>
                         {return x.name.toLowerCase(); }).indexOf(userCity);
-                        
+
                     if (cities[indexOfCity])  {
                         const cityName = cities[indexOfCity].name;
                         const cityLat = cities[indexOfCity].lat;
@@ -58,11 +59,50 @@ class HomePage extends Component {
         event.preventDefault()
 
         if (this.state.cityResult) {
+            const year = this.state.date.slice(0,4);
+            const month = this.state.date.slice(5,7);
+            const day = this.state.date.slice(8,10);
+            const hours = this.state.time.slice(0,2);
+            const minutes = this.state.time.slice(3,5);
+            const lat = this.state.cityLat;
+            const lng = this.state.cityLng;
+            var eventDate = new Date(year, month, day, hours, minutes);
+
+            timezoner.getTimeZone(
+                lat,
+                lng,
+                function (err, data) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        const toHHMMSS = (seconds) => {
+                            var sec_num = parseInt(seconds, 10); // don't forget the second param
+                            var hours   = Math.floor(sec_num / 3600);
+                            var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+                            var seconds = sec_num - (hours * 3600) - (minutes * 60);
+                        
+                            if (hours   < 10) {hours   = "0"+hours;}
+                            if (minutes < 10) {minutes = "0"+minutes;}
+                            if (seconds < 10) {seconds = "0"+seconds;}
+                            return hours+':'+minutes+':'+seconds;
+                        }
+                        console.log(data.timeZoneName);
+                        if (data.rawOffset >= 0) {
+                            console.log("UTC+" + toHHMMSS(data.rawOffset))
+                        } else {
+                            console.log("UTC-" + toHHMMSS(data.rawOffset).slice(2))
+                        }
+                        // UTC-6 hours
+                    }
+                },
+                { language: 'en', key: 'AIzaSyCe_cnkYOIgF2UqOd61VqPwSwtniXOb8J8' }
+            );
+
             const newEvent = {
                 name: this.state.name,
                 city: this.state.cityResult,
-                lat: this.state.cityLat,
-                lng: this.state.cityLng,
+                lat: lat,
+                lng: lng,
                 date: this.state.date,
                 time: this.state.time,
                 key: this.state.events.length + 1
