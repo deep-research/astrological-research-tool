e823417b7f42079d622a69781ae2834bcdd3be8d
Victor <deep-research>
2018-02-21 14:50:44 -0600

    Heroku 5

--- server/server.js
+++ server/server.js
@@ -18,9 +18,9 @@ app.use(bodyParser.json());
 
 // Send every request to the React app
 // Define any API routes before this runs
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 // Connect the API routes folder
 // require("../routes/api-routes.js")(app);
