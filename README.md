bda66e7179661d38441e322f02380e63a62dc021
Victor <deep-research>
2018-03-02 22:56:33 -0600

    Readme File Created

---
+++ README.md
@@ -0,0 +1,52 @@
+# The Astrological Research Tool
+
+* A full stacka astrological application built with React and Mongoose.
+[The Astrological Research Tool](https://astrological-research-tool.herokuapp.com/)
+
+## Getting Started
+
+* You can check out a working version of this project in the link above, or install it on your computer with the instructions below.
+
+### Prerequisites
+
+* Node.JS
+* Yarn Package Manager
+* Babel.JS
+* MongoDB
+
+### Installing
+
+1. `git clone` into your directory.
+2. `mongod` to start your Mongo server
+3. Create a new database in Mongo called `astrology`.
+4. `yarn install` in the project directory to prepare the necessary packages.
+5. `yarn build` to optimize the application.
+6. `npm install --save-dev babel-cli babel-preset-env` if you don't have Babel.JS working yet.
+7. `yarn start` to get it running.
+8. It will open in `localhost:3000`.
+
+## Deployment
+
+* Upload the project to Heroku and connect it to Mongo Lab Add-on.
+
+## Built With
+
+* [All Cities JSON by Randy Meech](https://gist.github.com/randymeech/5dabf9bf2831d9467ea6) - An array of objects containing global city names and coordinates.
+* [Timezoner](https://www.npmjs.com/package/timezoner) - Node.js client library for accessing Google Time Zone API.
+* [Moment Timezone](https://momentjs.com/timezone/) - Parse and display dates in any timezone.
+* [React Toastify](https://github.com/fkhadra/react-toastify) - React notification made easy!
+* [Bcrypt JS](https://www.npmjs.com/package/bcryptjs) - Optimized bcrypt in JavaScript with zero dependencies.
+* [Axios](https://www.npmjs.com/package/axios) - Promise based HTTP client for the browser and node.js.
+* [Express](https://www.npmjs.com/package/express) - Fast, unopinionated, minimalist web framework for node.js.
+* [Mongo](https://www.mongodb.com/) - A free and open-source cross-platform document-oriented database program.
+* [Mongoose](https://www.npmjs.com/package/mongoose) - a MongoDB object modeling tool designed to work in an asynchronous environment.
+* [React](https://reactjs.org/) - Frontend Framework by Facebook.
+* [Node.js](https://nodejs.org/en/) - A JavaScript runtime built on Chrome's V8 JavaScript engine.
+* [Yarn](https://www.npmjs.com/package/yarn) - A fast, reliable, and secure dependency management.
+* [Babel](https://babeljs.io/) - A compiler for writing ECMAScript 2015 JavaScript.
+* [README-Template](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2) - A template to make good README.md.
+
+
+## Author
+
+* [Deep Research](https://github.com/deep-research)
--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -8,29 +8,90 @@ class AboutSection extends Component {
             <br />
             <h3>About</h3>
             <p>
-                The purpose of this project was to propose a system of <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Metaphysics">metaphysical</a> research that studies time in more practical manner when compared to the <a href="https://en.wikipedia.org/wiki/Mythology" target="_blank" rel="noopener noreferrer">mythologically</a> based methods of <a rel="noopener noreferrer" target="_blank" href="https://en.wikipedia.org/wiki/Horoscopic_astrology">horoscopic astrology</a>. Once the basic facts about events are established, various methods of interpretation could then be tested. The current design of this website was a valuable exercise in reimagining the study of time. Some of the changes that were implemented are as follows:
+                The purpose of this project was to propose a system of <a
+                    target="_blank" rel="noopener noreferrer"
+                    href="https://en.wikipedia.org/wiki/Metaphysics"
+                    >metaphysical</a> research that studies time in more
+                practical manner when compared to the <a
+                    href="https://en.wikipedia.org/wiki/Mythology"
+                    target="_blank" rel="noopener noreferrer"
+                    >mythologically</a> based methods of <a
+                    rel="noopener noreferrer" target="_blank"
+                    href="https://en.wikipedia.org/wiki/Horoscopic_astrology"
+                    >horoscopic astrology</a>.
+                Once the basic facts about events are established,
+                various methods of interpretation could then be tested.
+                The current design of this website was a valuable exercise
+                in reimagining the study of time. Some of the changes that
+                were implemented are as follows:
             </p>
             <p>
                 <ul>
                     <li>
-                        The <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/House_system">house system</a> was removed in favor of classifications for <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Diurnal_motion">diurnal motion</a>. These catagories describe whether an object is above or below the horizon, and whether an object is ascending or descending in terms of <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Horizontal_coordinate_system">altitude</a>.
+                        The <a target="_blank" rel="noopener noreferrer"
+                            href="https://en.wikipedia.org/wiki/House_system"
+                            >house system</a> was removed in favor of
+                        classifications for <a target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://en.wikipedia.org/wiki/Diurnal_motion"
+                        >diurnal motion</a>. These catagories describe whether
+                        an object is above or below the horizon, and whether an
+                        object is ascending or descending in terms of <a
+                            target="_blank" rel="noopener noreferrer"
+                            href="https://en.wikipedia.org/wiki/Horizontal_coordinate_system"
+                            >altitude</a>.
                     </li>
                     <li>
-                        The <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Zodiac">zodiac </a>system was removed in favor of four seasonally based <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Circular_sector">sectors</a>, although planets could still be seen as passing through these sectors on the <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Ecliptic">ecliptic</a>.
+                        The <a target="_blank" rel="noopener noreferrer"
+                            href="https://en.wikipedia.org/wiki/Zodiac"
+                        >zodiac</a> system was removed in favor of four
+                        seasonally based <a target="_blank" rel="noopener noreferrer"
+                            href="https://en.wikipedia.org/wiki/Circular_sector"
+                        >sectors</a>, although planets could still be seen as
+                        passing through these sectors on the <a
+                            target="_blank" rel="noopener noreferrer"
+                            href="https://en.wikipedia.org/wiki/Ecliptic"
+                            >ecliptic</a>.
                     </li>
                     <li>
-                        <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Lunar_phase">Lunar phases</a> are used instead of <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Lunar_mansion">lunar mansions</a>.
+                        <a target="_blank" rel="noopener noreferrer"
+                            href="https://en.wikipedia.org/wiki/Lunar_phase"
+                        >Lunar phases</a> are used instead of <a
+                            target="_blank" rel="noopener noreferrer"
+                            href="https://en.wikipedia.org/wiki/Lunar_mansion"
+                        >lunar mansions</a>.
                     </li>
                     <li>
-                        There are no <a target="_blank" rel="noopener noreferrer" href="http://www.chrisbrennanastrologer.com/Brennan-Theoretical-Rationale.pdf">hypothetical points</a> such as the <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Arabic_parts">arabic parts</a> and there is no mention of <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Astrological_aspect">planetary aspects</a>.
+                        There are no <a target="_blank" rel="noopener noreferrer"
+                            href="http://www.chrisbrennanastrologer.com/Brennan-Theoretical-Rationale.pdf"
+                        >hypothetical points</a> such as the <a target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://en.wikipedia.org/wiki/Arabic_parts"
+                        >arabic parts</a> and there is no mention of <a
+                            target="_blank" rel="noopener noreferrer"
+                            href="https://en.wikipedia.org/wiki/Astrological_aspect"
+                        >planetary aspects</a>.
                     </li>
                 </ul>
             </p>           
             <p>
-                The conclusion I came to towards the end of this project was that the changes were insufficient. Some problems that are still being inherited from traditional astrology are the focus on exact moments in time and astronomical correlations instead of the events themselves. I actually think studying time as a symbolic landscape may be a far better approach. We already do this with our history when we talk about mythic periods such as the <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Roaring_Twenties">Roaring Twenties</a> and the <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/American_Century">American Century</a>. Instead of removing the myth altogether, we may need to learn how to discover the myth for ourselves.
+                The conclusion I came to towards the end of this project was that the
+                changes were insufficient. Some problems that are still being inherited
+                from traditional astrology are the focus on exact moments in time and
+                astronomical correlations instead of the events themselves. I actually
+                think studying time as a symbolic landscape may be a far better approach.
+                We already do this with our history when we talk about mythic periods
+                such as the <a target="_blank" rel="noopener noreferrer"
+                    href="https://en.wikipedia.org/wiki/Roaring_Twenties"
+                >Roaring Twenties</a> and the <a target="_blank" rel="noopener noreferrer"
+                    href="https://en.wikipedia.org/wiki/American_Century"
+                >American Century</a>. Instead of removing the myth altogether,
+                we may need to learn how to discover the myth for ourselves.
             </p>
             <p>
-                For more information: <a href="https://github.com/deep-research/astrological-research-tool" target="_blank" rel="noopener noreferrer">Github Repository</a>
+                For more information: <a
+                    href="https://github.com/deep-research/astrological-research-tool"
+                    target="_blank" rel="noopener noreferrer">Github Repository</a>.
             </p>
         </div>
         );
